e05031637d3489cd75a097167f0b5bfbd57b6634
Abdulla <abdusattorovabdulla60@gmail.com>
2025-04-04 10:45:24 +0500

    full

--- uyga_vazifa/src/book/book.service.ts
+++ uyga_vazifa/src/book/book.service.ts
@@ -1,32 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { CreateBookDto } from "./dto/create-book.dto";
+import { UpdateBookDto } from "./dto/update-book.dto";
 
 @Injectable()
 export class BookService {
   constructor(private prisma: PrismaService) {}
 
-  create(dto: CreateBookDto) {
-    return this.prisma.book.create({ data: dto });
+  async create(dto: CreateBookDto) {
+    return await this.prisma.book.create({ data: dto });
   }
 
-  findAll() {
-    return this.prisma.book.findMany();
+  async findAll() {
+    return await this.prisma.book.findMany();
   }
 
-  findOne(id: string) {
-    return this.prisma.book.findUnique({ where: { id } });
+  async findOne(id: string) {
+    return await this.prisma.book.findUnique({ where: { id } });
   }
 
-  update(id: string, dto: UpdateBookDto) {
-    return this.prisma.book.update({
+  async update(id: string, dto: UpdateBookDto) {
+    return await this.prisma.book.update({
       where: { id },
       data: dto,
     });
   }
 
-  remove(id: string) {
-    return this.prisma.book.delete({ where: { id } });
+  async remove(id: string) {
+    return await this.prisma.book.delete({ where: { id } });
   }
 }
